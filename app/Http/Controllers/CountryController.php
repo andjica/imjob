094b74ee49310d232f924cf59d84379afee76b8b
Danny Milosevic <andjaaa95@gmail.com>
2025-01-01 16:18:15 +0100

    adding currency

---
+++ app/Http/Controllers/CountryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Interfaces\CountryInterface;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    protected $countryServices;
+
+    public function __construct(CountryInterface $countryServices)
+    {
+        $this->countryServices = $countryServices;
+    }
+
+    public function getCurrency($countryId)
+    {
+        $currency = $this->countryServices->getCurrency($countryId);
+
+        return $currency;
+    }
+}
